95bd5b0504499915a77496108634dbb30b5cde58
IrinAtanasoV <32416410+IrinAtanasoV@users.noreply.github.com>
2017-09-30 22:33:32 +0300

    Upload files.

---
+++ Foreign Languages/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foreign_Languages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string language = Console.ReadLine();
+
+            if (language == "USA" || language == "England")
+            {
+                Console.WriteLine("English");
+
+            }
+            else if (language == "Spain" || language == "Mexico" || language == "Argentina")
+            {
+                Console.WriteLine("Spanish");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
