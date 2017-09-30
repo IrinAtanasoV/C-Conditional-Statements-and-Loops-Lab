ae772bd467966d01e2c343399fa149b1b8185b8c
IrinAtanasoV <32416410+IrinAtanasoV@users.noreply.github.com>
2017-09-30 23:27:47 +0300

    Upload files.

---
+++ Multiplication Table 2.0/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (num2 > 10)
+            {
+                Console.WriteLine($"{num1} X {num2} = {num1 * num2}");
+            }
+            else
+            {
+                for (int i = num2; i <= 10; i++)
+                {
+                    Console.WriteLine($"{num1} X {num2} = {num1 * num2}");
+                    num2++;
+                }
+            }
+        }
+    }
+}
---
+++ Multiplication Table/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            
+
+           
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine($"{num1} X {i} = {num1 * i}");
+                    
+                }
+            
+        }
+    }
+}
