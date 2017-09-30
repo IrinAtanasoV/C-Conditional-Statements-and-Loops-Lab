cf4b31175806fe4b1023ed4b96cf3d19cb2334c7
IrinAtanasoV <32416410+IrinAtanasoV@users.noreply.github.com>
2017-09-30 23:15:12 +0300

    Upload files.

---
+++ Sum of Odd Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_Odd_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                    sum = sum + i;
+                    number = number + 1;
+                }
+                
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
