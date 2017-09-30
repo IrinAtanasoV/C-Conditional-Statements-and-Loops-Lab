b1f47480605eaacfd13082b78bc02139ee50ad37
IrinAtanasoV <32416410+IrinAtanasoV@users.noreply.github.com>
2017-09-30 21:52:45 +0300

    Upload files.

---
+++ Back in  30 Minutes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Back_in__30_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes = minutes + 30;
+            
+            {
+
+            }
+            if (minutes > 59)
+            {
+                minutes = minutes - 60;
+                hours++;
+                
+                if (hours == 24)
+                {
+                    hours = 0;
+                }
+                Console.WriteLine($"{hours}:{minutes:D2}");
+            }
+            else
+            {
+                if (hours == 24)
+                {
+                    hours = 0;
+                }
+                Console.WriteLine($"{hours}:{minutes:D2}");
+            }
+
+
+        }
+    }
+}
